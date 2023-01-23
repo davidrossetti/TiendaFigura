44037b6ddc6f88326bb53fa918f62ba97c52729c
Pedro David Rossetti <davidk8722@gmail.com>
2023-01-22 22:32:23 -0300

    se modifica js para agregar carrusel comandos de JS ligados a la validacion de datos del formulario

--- index.js
+++ index.js
@@ -2,35 +2,35 @@
 
 
 // ///////////////BOTON Y MANTENIMIENTO DEL MODO OSCURO///
-// const botonModos= document.querySelector("#claro-oscuro")
-// const body=document.querySelector(".modo-claro")
+const botonModos= document.querySelector("#claro-oscuro")
+const body=document.querySelector(".modo-claro")
 
-// ///Se guarda un valor en localStore, si es verdadero,se 
-// // mantiene el modo oscuro(por si se reinicia el navegador).
+///Se guarda un valor en localStore, si es verdadero,se 
+// mantiene el modo oscuro(por si se reinicia el navegador).
 
-// function guardadoPaginaModoOscuro(){
-//   if (localStorage.getItem("paginaModoOscuro")==="true") {
-//     body.classList.toggle("modo-oscuro")
-//     botonModos.textContent="Modo claro"
-//   }
-// }
-// //Boton para cambio al modo oscuro, se guarda un valor en localStore
-// //para guardar el cambio, si se reinicia el navegador.
+function guardadoPaginaModoOscuro(){
+  if (localStorage.getItem("paginaModoOscuro")==="true") {
+    body.classList.toggle("modo-oscuro")
+    botonModos.textContent="Modo claro"
+  }
+}
+//Boton para cambio al modo oscuro, se guarda un valor en localStore
+//para guardar el cambio, si se reinicia el navegador.
 
-// botonModos.onclick=() => {
-//     body.classList.toggle("modo-oscuro")
-//     if(body.className ==="modo-claro"){
-//         botonModos.textContent="Modo Oscuro"
-//         localStorage.setItem("paginaModoOscuro",false)
-//     }else{
-//         botonModos.textContent="Modo claro"
-//         localStorage.setItem("paginaModoOscuro",true)
+botonModos.onclick=() => {
+    body.classList.toggle("modo-oscuro")
+    if(body.className ==="modo-claro"){
+        botonModos.textContent="Modo Oscuro"
+        localStorage.setItem("paginaModoOscuro",false)
+    }else{
+        botonModos.textContent="Modo claro"
+        localStorage.setItem("paginaModoOscuro",true)
                
-//     }
+    }
     
-// }
-// //Ejecuta el metodo de guardado
-// guardadoPaginaModoOscuro()
+}
+//Ejecuta el metodo de guardado
+guardadoPaginaModoOscuro()
 
 
 
